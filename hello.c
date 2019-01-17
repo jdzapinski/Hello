12aab938fef89f720dc0fe05d5c935a6dfb9bccf
Joseph Zapinski <jdzapinski@gmail.com>
2019-01-17 08:14:17 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+*  Author: Chris Bourke
+*  Date: 2016/11/02
+**  A simple hello world program in C
+**/
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Advertising\n");
+
+    return 0;
+    }
